b9b875632cd18580970c3abf64e6414d64c38453
manupanand <manupanandindianinvestor@gmail.com>
2026-03-05 19:07:18 +0000

    master node tasks

--- kubernets-cluster/ansible/run.sh
+++ kubernets-cluster/ansible/run.sh
@@ -1,3 +1,4 @@
- ansible-playbook -i inventory/hosts.ini playbook.yml --tags $1  --ask-become-pass
- ansible-playbook -i inventory/hosts.ini playbook.yml --tags firewalld -v
+ansible-playbook -i inventory/hosts.ini playbook.yml --tags $1  --ask-become-pass
+ansible-playbook -i inventory/hosts.ini playbook.yml --tags firewalld -v
 ansible-playbook -i inventory/hosts.ini playbook.yml --tags k8s-common -v
+ansible-playbook -i inventory/hosts.ini playbook.yml --tags k8s-master -v

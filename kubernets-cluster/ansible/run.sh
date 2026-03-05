d12363f385ed87a357d71e4c7f2a3c769177cebf
manupanand <manupanandindianinvestor@gmail.com>
2026-03-05 19:22:10 +0000

    worker node tasks edit

--- kubernets-cluster/ansible/run.sh
+++ kubernets-cluster/ansible/run.sh
@@ -2,7 +2,7 @@ ansible-playbook -i inventory/hosts.ini playbook.yml --tags $1  --ask-become-pas
 ansible-playbook -i inventory/hosts.ini playbook.yml --tags firewalld -v
 ansible-playbook -i inventory/hosts.ini playbook.yml --tags k8s-common -v
 ansible-playbook -i inventory/hosts.ini playbook.yml --tags k8s-master -v
-
+ansible-playbook -i inventory/hosts.ini playbook.yml --tags k8s-worker -v
 
 # join command
 # "kubeadm join 192.168.1.28:6443 --token 9av5ew.ic10oh0abtpjtdc2 --discovery-token-ca-cert-hash sha256:978050961c62c7ab65993c6e89fc09b524f3bcf0016c2bf0f6d695c6385a287a

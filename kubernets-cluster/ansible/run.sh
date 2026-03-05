1d91b04b2105b337b125eadaef8f557eb1fbbf42
manupanand <manupanandindianinvestor@gmail.com>
2026-03-05 18:57:48 +0000

    added common tasks

--- kubernets-cluster/ansible/run.sh
+++ kubernets-cluster/ansible/run.sh
@@ -1,2 +1,3 @@
  ansible-playbook -i inventory/hosts.ini playbook.yml --tags $1  --ask-become-pass
  ansible-playbook -i inventory/hosts.ini playbook.yml --tags firewalld -v
+ansible-playbook -i inventory/hosts.ini playbook.yml --tags k8s-common -v
